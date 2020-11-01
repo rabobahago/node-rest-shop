595f5ee97dff767454484a104c770c5471f79e04
rabobahago <rabobahago@gmail.com>
2020-11-01 08:27:10 +0100

    save to the database

--- api/routes/products.js
+++ api/routes/products.js
@@ -29,11 +29,16 @@ router.post('/', (req, res, next) => {
 
 router.get('/:productsId', (req, res, next) => {
   const id = req.params.productsId
-  if (id === 'special') {
-    res.status(200).json({ message: 'you did discovered the special id', id })
-  } else {
-    res.status(200).json({ message: 'you passed an Id' })
-  }
+  Product.findById(id)
+    .exec()
+    .then((doc) => {
+      console.log(doc)
+      res.status(200).json(doc)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ error: err })
+    })
 })
 
 router.patch('/:productsId', (req, res, next) => {
